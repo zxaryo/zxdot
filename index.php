3f4b9e0931af0ae7ae9f5d8acf675a39067870b9
Akhil Chandran <zxaryo@gmail.com>
2016-12-03 12:24:16 +0530

    Basic

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "localhost";
+$username = "u595707791_zxdot";
+$password = "u595707791_zxdot";
+$dbname = "u595707791_zxdot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO zxdot (sender, value) VALUES ('" + $_POST['sender'] +"', '"+ $_POST['value'] +"')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
