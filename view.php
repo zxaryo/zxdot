3ea1ce203d2f84162e0ee8c6309d688c59c414f2
Akhil Chandran <zxaryo@gmail.com>
2016-12-04 00:11:39 +0530

    1

--- view.php
+++ view.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo  $row["timesamp"]. "| " $row["sender"].  " : "$row["value"]. "<br>";
+        echo  $row["timesamp"]. "| " .$row["sender"].  " : " . $row["value"]. "<br>";
     }
 } else {
     echo "0 results";
