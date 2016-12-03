a926d34b7c5c87fee11f65fb146c98195ab4a041
Akhil Chandran <zxaryo@gmail.com>
2016-12-03 12:32:30 +0530

    edfyikl

--- index.php
+++ index.php
@@ -12,9 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO zxdot (sender, value) VALUES ('" + $_POST['sender'] +"', '"+ $_POST['value'] +"')";
-
-echo $sql;
+echo $sql = "INSERT INTO zxdot (sender, value) VALUES ('" + $_POST['sender'] +"', '"+ $_POST['value'] +"')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
