0e62561a9af73474d2ddd43e4b4f340c1d7c5324
Akhil Chandran <zxaryo@gmail.com>
2017-01-14 07:26:01 +0530

    inspirational '

--- update.php
+++ update.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "UPDATE zxdot SET value=".$_GET['value']." WHERE sender=".$_GET['sender']."";
+$sql = "UPDATE zxdot SET value=".$_GET['value']." WHERE sender=' ".$_GET['sender']." ' ";
 
 //echo $sql;
 
